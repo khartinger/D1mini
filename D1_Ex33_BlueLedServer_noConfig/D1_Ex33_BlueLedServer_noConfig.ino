fa0056bd6bce2ffc972e62274c24ebee31532c8e
Karl Hartinger <karl@hartingers.at>
2017-04-09 16:57:48 +0200

    Simple server to switch blue D1mini led by request
    Simple server to switch blue D1mini led with fix wlan data from file
    myWLAN.h

---
+++ D1_Ex33_BlueLedServer_noConfig/D1_Ex33_BlueLedServer_noConfig.ino
@@ -0,0 +1,81 @@
+//_____D1_Ex33_BlueLedServer_noConfig.ino_____170409-170409_____
+// Host listens to Port 80 for request /on or /off to turn
+//  blue D1 mini LED on or off
+// On boot up blue led is on, when connected to wlan led is off
+// Answer to request: plain text OK
+// Serial monitor: log messages (9600Bd)
+// Hardware: WeMos D1 Mini
+#define PIN_LED_BLUE D4
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include "myWLAN.h"
+ESP8266WebServer server(80);
+
+//_____on http-request: turn blue led on________________________
+void handleOn(void)
+{
+ digitalWrite(PIN_LED_BLUE, 0);   // blue LED on (!)
+ server.send(200, "text/plain", "OK");
+}
+
+//_____on http-request: turn blue led off_______________________
+void handleOff(void)
+{
+ digitalWrite(PIN_LED_BLUE, 1);   // blue LED off (!)
+ server.send(200, "text/plain", "OK");
+}
+
+//_____on http-request without parameter________________________
+void handleRoot(void)
+{
+ server.send(200, "text/plain", "Blue led server ready!");
+}
+
+//_____setup wlan_______________________________________________
+void setup_wlan()
+{
+ int i=50;
+ while(i==50)
+ {
+  WiFi.disconnect();
+  delay(100);
+  WiFi.config(ip, gateway, subnet);  // skipp for dhcp
+  WiFi.begin(ssid_, pass_);
+  //.....wait until connected to WLAN (access point)............
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+   delay(200);
+   Serial.print(".");
+   i--; if(i<1) { i=50; Serial.println(""); }
+  }
+ }  
+}
+
+//_____setup routine____________________________________________
+void setup(void){
+ //-----start serial interface----------------------------------
+ Serial.begin(9600);
+ Serial.println("");
+ //-----turn blue LED on----------------------------------------
+ pinMode(PIN_LED_BLUE, OUTPUT);
+ digitalWrite(PIN_LED_BLUE, 0);   // blue LED on (!)
+ //-----setup WLAN----------------------------------------------
+ setup_wlan();
+ //-----connected - turn blue LED off---------------------------
+ digitalWrite(PIN_LED_BLUE, 1);   // blue LED off (!)
+ //-----handles to answer the request---------------------------
+ server.on("/", handleRoot);
+ server.on("/on", handleOn);
+ server.on("/off", handleOff);
+ //-----start server--------------------------------------------
+ server.begin();
+ Serial.println("\nHTTP server started");
+}
+
+//_____main loop________________________________________________
+void loop(void){
+ server.handleClient();
+}
